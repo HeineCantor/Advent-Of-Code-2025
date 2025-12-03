600b26b259fca7e3208090d281101bd05c1f92ab
HeineCantor <carmine.palmese@gmail.com>
2025-12-03 09:40:55 +0100

    day 2 solved

--- day_2/main.c
+++ day_2/main.c
@@ -4,17 +4,36 @@
 #include <string.h>
 
 #define INPUT_FILE_PATH "input.txt"
+#define MULTIPLE_SUBSTRING_CHECK true
 
-bool has_double_substring(char* input, char* substring) {
+bool has_double_substring(char* input) {
     if (strlen(input) % 2 != 0)
         return false;
 
     size_t half_len = strlen(input) / 2;
     
     if (strncmp(input, input + half_len, half_len) == 0)
-    {
-        strncpy(substring, input, half_len);
         return true;
+
+    return false;
+}
+
+bool has_multiple_substring(char* input) {
+    size_t half_lenf = strlen(input) / 2;
+
+    for (size_t len = 1; len <= half_lenf; len++) {
+        if (strlen(input) % len != 0)
+            continue;
+            
+        int check_num = strlen(input) / len;
+
+        for (int i = 1; i < check_num; i++) {
+            if (strncmp(input, input + i * len, len) != 0)
+                break;
+
+            if (i == check_num - 1)
+                return true;
+        }
     }
 
     return false;
@@ -30,29 +49,30 @@ int main() {
     char buffer[4096];
     char* substrings = strtok(fgets(buffer, sizeof(buffer), file), ",");
 
-    int totalSum = 0;
+    long totalSum = 0;
 
     for (; substrings != NULL; substrings = strtok(NULL, ",")) {
         char* dashPtr = strchr(substrings, '-');
         
-        int rangeStart = atoi(strndup(substrings, dashPtr - substrings));
-        int rangeEnd = atoi(dashPtr + 1);
+        long rangeStart = atol(strndup(substrings, dashPtr - substrings));
+        long rangeEnd = atol(dashPtr + 1);
 
-        for(int i = rangeStart; i <= rangeEnd; i++)
+        for(long i = rangeStart; i <= rangeEnd; i++)
         {
             char numString[256];
-            sprintf(numString, "%d", i);
+            sprintf(numString, "%ld", i);
 
-            char tmp_buffer[256];
-            if (has_double_substring(numString, tmp_buffer))
+            bool check_condition = MULTIPLE_SUBSTRING_CHECK ? has_multiple_substring(numString) : has_double_substring(numString);
+
+            if (check_condition)
             {
                 totalSum+=i;
-                printf("Found double substring: %s in number: %s\n", substrings, numString);
+                printf("Found substring matching: %s in number: %s\n", substrings, numString);
             }
         }
     }
 
-    printf("\nTotal Sum: %d\n", totalSum);
+    printf("\nTotal Sum: %ld\n", totalSum);
 
     return 0;
 }
